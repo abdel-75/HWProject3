5a291649308b53c2253cc64a210695795041e28c
Abdessalam <Abdessalam@192.168.148.117>
2023-04-07 20:09:34 +0000

    third commit

---
+++ src/hwtest3/DemoProject4.java
@@ -0,0 +1,13 @@
+package hwtest3;
+
+public class DemoProject4 {
+	
+	public static void main(String[] args) {
+		System.out.println(" this is demo project3");
+		
+		
+		
+		System.out.println(" hello the world this is my project");
+	}
+
+}
